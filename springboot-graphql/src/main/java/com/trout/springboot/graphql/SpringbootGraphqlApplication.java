3a1740626ce5901e51c763a3d6d465fc6de032a6
trout <trout365247@gmail.com>
2020-04-27 00:43:53 +0530

    First commit - Base Spring Boot

---
+++ springboot-graphql/src/main/java/com/trout/springboot/graphql/SpringbootGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.trout.springboot.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlApplication.class, args);
+	}
+
+}
